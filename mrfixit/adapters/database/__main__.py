aee9f42395156083603ee59af5fb32f3b28b1f14
Ertiops <straxisrule@gmail.com>
2025-06-23 15:21:20 +0300

    Added ci

--- mrfixit/adapters/database/__main__.py
+++ mrfixit/adapters/database/__main__.py
@@ -1,26 +1,10 @@
 import argparse
 import logging
-from dataclasses import dataclass, field
-from os import environ
 
 from alembic.config import CommandLine
 
+from mrfixit.adapters.database.config import DatabaseConfig
 from mrfixit.adapters.database.utils import make_alembic_config
-
-
-@dataclass(frozen=True, kw_only=True, slots=True)
-class DatabaseConfig:
-    user: str = field(default_factory=lambda: environ.get("APP_DB_USER", "mrfixit"))
-    password: str = field(
-        default_factory=lambda: environ.get("APP_DB_PASSWORD", "mrfixit")
-    )
-    name: str = field(default_factory=lambda: environ.get("APP_DB_NAME", "mrfixit"))
-    host: str = field(default_factory=lambda: environ.get("APP_DB_HOST", "127.0.0.1"))
-    port: int = field(default_factory=lambda: int(environ.get("APP_DB_PORT", 5432)))
-
-    @property
-    def dsn(self) -> str:
-        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
 
 
 def main() -> None:
