c688ad35075aa945c874e3b570e772378822a8c6
Elijah Tomin <104155685+Ertiops@users.noreply.github.com>
2025-11-09 14:55:55 +0300

    added logging of chat middleware (#17)

--- mrfixit/presenters/bot/middlewares/group.py
+++ mrfixit/presenters/bot/middlewares/group.py
@@ -1,8 +1,11 @@
+import logging
 from collections.abc import Awaitable, Callable
 from typing import Any
 
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject, Update
+
+log = logging.getLogger(__name__)
 
 
 class GroupFilterMiddleware(BaseMiddleware):
@@ -18,11 +21,12 @@ class GroupFilterMiddleware(BaseMiddleware):
         message = event.message or (
             event.callback_query.message if event.callback_query else None
         )
-
         if not message:
             return await handler(event, data)
 
         chat_id = message.chat.id
+        log.info("GroupFilterMiddleware: chat_id=%s", chat_id)
+        log.info("GroupFilterMiddleware: allowed_chat_id=%s", self.allowed_chat_id)
         if chat_id != self.allowed_chat_id:
             if event.message:
                 await event.message.answer(
